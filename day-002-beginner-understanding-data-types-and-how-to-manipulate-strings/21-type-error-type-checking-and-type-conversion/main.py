7769e3fe646fd228e3970dc45d36f18409e7628a
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-01 00:02:24 -0300

    :construction: 002-21:Type error, type checking and type conversion

---
+++ day-002-beginner-understanding-data-types-and-how-to-manipulate-strings/21-type-error-type-checking-and-type-conversion/main.py
@@ -0,0 +1,13 @@
+# num_char = len(input("What is your name? "))
+
+# print(type(num_char))
+
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.35"))
+print(str(70) + str(100))
