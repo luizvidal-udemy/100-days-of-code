4c58a947ccc2716da160a1c749a5abfcad60b5fc
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-01 09:46:57 -0300

    :construction: 002-25:Number manipulation and f strings in python

---
+++ day-002-beginner-understanding-data-types-and-how-to-manipulate-strings/25-number-manipulation-and-f-strings-in-python/main.py
@@ -0,0 +1,14 @@
+print(round(2.666666666, 2))
+print(8 // 3) # Floor Division
+
+score = 0
+# User scores a point
+# -= += *= /=
+score += 1 # score = score + 1 
+print(score)
+height = 1.8
+isWinning = True
+
+print(
+  f"Your score is {score}, your heigth is {height}, you are winning is {isWinning}"
+)
