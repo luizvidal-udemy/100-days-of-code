12a994fe0867f99ff11c769421346d1f100ba503
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-11 16:45:59 -0300

    :construction: 004-31:Random Module

---
+++ day-004-beginner-randomisation-and-python-lists/01-random-module/main.py
@@ -0,0 +1,21 @@
+import random
+import my_module
+
+# random_integer = random.randint(1, 10)
+# favorite_number = my_module.my_favourite_number
+
+# print(random_integer)
+# print(favorite_number)
+
+# random_number_0_to_1 = random.random() * 10
+# random_float = random.uniform(1, 10)
+
+# print(random_number_0_to_1)
+# print(random_float)
+
+heads_or_tails = random.randint(0, 1)
+
+if heads_or_tails == 1:
+  print("Heads")
+else:
+  print("Tails")
