aff0f7bb6655633e2b41742cd7c0fee6372b175a
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-15 18:05:45 -0300

    :construction: 009-69:Nesting List and Dictionaries

---
+++ day-009-beginner-dictionaries-nesting/03-nesting-list-and-dictionaries/main.py
@@ -0,0 +1,31 @@
+capitals = {"France": "Paris", "Germany": "Berlin"}
+
+# Nested List in Dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Stuttgart", "Berlin",],
+}
+
+# Print Lille
+print(travel_log["France"][1])
+
+nested_list = ["A", "B", ["C", "D"]]
+
+# Print D
+print(nested_list[2][1])
+
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 8,
+    },
+
+    "Germany": {
+        "cities_visited": ["Stuttgart", "Berlin"],
+        "total_visits": 5,
+    },
+}
+
+# Print Stuttgart
+print(travel_log["Germany"]["cities_visited"][0])
