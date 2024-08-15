ffcba622a9b0a22c8dade86a79cf4d6994cacc36
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-14 21:17:31 -0300

    :construction: 008-62:Positional vs. Keyword Arguments

---
+++ day-008-beginner-function-parameters/03-positional-vs-keyword-arguments/main.py
@@ -0,0 +1,11 @@
+# Functions with more than one input
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+
+greet_with("Luiz Vidal", "Brazil")
+
+greet_with(name="Luiz Vidal 2", location="Brazil 2")
