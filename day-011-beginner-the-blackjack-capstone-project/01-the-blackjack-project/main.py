6097f503747f3c93a2f0756eddb6774bd4f75b15
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-18 17:13:36 -0300

    :construction: 011-80:Hint 6-8 Solution Walkthrough

--- day-011-beginner-the-blackjack-capstone-project/01-the-blackjack-project/main.py
+++ day-011-beginner-the-blackjack-capstone-project/01-the-blackjack-project/main.py
@@ -14,3 +14,15 @@ computer_cards = []
 for _ in range(2):
     user_cards.append(deal_cards())
     computer_cards.append(deal_cards())
+
+
+def calculate_score(cards):
+    if sum(cards) and len(cards) == 2:
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    
+    return sum(cards)
