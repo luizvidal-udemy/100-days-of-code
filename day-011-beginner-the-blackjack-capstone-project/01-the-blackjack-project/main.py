fdaca0c896231e80bebde64275612823566d6fbc
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-18 17:19:59 -0300

    :construction: 011-82:Hint 9 Solution Walkthrough

--- day-011-beginner-the-blackjack-capstone-project/01-the-blackjack-project/main.py
+++ day-011-beginner-the-blackjack-capstone-project/01-the-blackjack-project/main.py
@@ -8,21 +8,32 @@ def deal_cards():
     return card
 
 
+def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 for _ in range(2):
     user_cards.append(deal_cards())
     computer_cards.append(deal_cards())
 
 
-def calculate_score(cards):
-    if sum(cards) and len(cards) == 2:
-        return 0
-    
-    if 11 in cards and sum(cards) > 21:
-        cards.remove(11)
-        cards.append(1)
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
 
-    
-    return sum(cards)
+print(f"Your cards: {user_cards}, current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
