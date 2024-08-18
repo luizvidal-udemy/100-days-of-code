63cb1366ec6acebb2fe6f1ad3badabef469fb82d
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-18 17:40:21 -0300

    :construction: 011-83:Hint 12 Solution Walkthrough

--- day-011-beginner-the-blackjack-capstone-project/01-the-blackjack-project/main.py
+++ day-011-beginner-the-blackjack-capstone-project/01-the-blackjack-project/main.py
@@ -1,7 +1,7 @@
 import random
 
 
-def deal_cards():
+def deal_card():
     """Returns a random ard from the deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
@@ -21,19 +21,35 @@ def calculate_score(cards):
 
 
 user_cards = []
+user_score = -1
 computer_cards = []
+computer_score = -1
 is_game_over = False
 
 for _ in range(2):
-    user_cards.append(deal_cards())
-    computer_cards.append(deal_cards())
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
 
-print(f"Your cards: {user_cards}, current score: {user_score}")
-print(f"Computer's first card: {computer_cards[0]}")
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+    else:
+        user_should_deal = input(
+            "Type 'y' to get another card, type 'n' to pass: ")
+
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
