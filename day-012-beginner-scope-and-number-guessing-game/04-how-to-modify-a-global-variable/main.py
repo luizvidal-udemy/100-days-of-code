18579f012e43f454988357fcd5f8a5ae02433dd0
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-18 21:41:32 -0300

    :construction: 012-89:How to Modify a Global Variable

---
+++ day-012-beginner-scope-and-number-guessing-game/04-how-to-modify-a-global-variable/main.py
@@ -0,0 +1,12 @@
+# Modifying Global Scope
+enemies = 1
+
+
+def increase_enemies(enemy):
+    print(f"enemies inside function: {enemies}")
+    return enemy + 1
+
+
+enemies = increase_enemies(enemies)
+
+print(f"enemies outside function: {enemies}")
