faaa2ed90bb0e2e38100a3dfd58cf8200ac0d550
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-08-19 01:01:50 -0300

    :construction: 013-94:Reproduce the Bug

---
+++ day-013-beginner-debugging-how-to-find-and-fix-errors-in-your-code/01-describe-the-problem/main.py
@@ -0,0 +1,11 @@
+def my_function():
+    for i in range(1,20):
+        if i == 20:
+            print("You got it")
+
+my_function()
+
+# Describe the Problem - Write you answers as comments:
+# 1. What is the for loop doing?
+# 2. Wen is the function meant to print "You got it"?
+# 3. What are you assumptions about the value of i?
