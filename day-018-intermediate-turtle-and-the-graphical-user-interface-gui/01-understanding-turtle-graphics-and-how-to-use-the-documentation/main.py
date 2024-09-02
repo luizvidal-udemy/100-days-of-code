0a0826b834097784bbc9f20b6625e03e3cfb415f
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-01 21:31:47 -0300

    :construction: 018-128:Understanding Turtle Graphics and How to use the Documentation

---
+++ day-018-intermediate-turtle-and-the-graphical-user-interface-gui/01-understanding-turtle-graphics-and-how-to-use-the-documentation/main.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.forward(90)
+
+
+screen = Screen()
+screen.exitonclick()
+
+
+# Turtle Star
+# from turtle import *
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
+# end_fill()
+# done()
+# screen = Screen()
+# screen.exitonclick()
