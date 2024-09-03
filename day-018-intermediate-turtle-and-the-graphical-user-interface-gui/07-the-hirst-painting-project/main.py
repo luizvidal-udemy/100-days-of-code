f2c796a13b77011adc9a4892b56ebcae3b567805
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-02 22:35:51 -0300

    :construction: 018-137:The Hirst Painting Project Part 2 - Drawing the Dots

--- day-018-intermediate-turtle-and-the-graphical-user-interface-gui/07-the-hirst-painting-project/main.py
+++ day-018-intermediate-turtle-and-the-graphical-user-interface-gui/07-the-hirst-painting-project/main.py
@@ -1,18 +1,40 @@
+from turtle import Turtle, Screen, colormode, bgcolor
+from random import choice
 from colorgram import extract
 
 
-rgb_colors = []
+turtle = Turtle()
+turtle.speed("fastest")
+turtle.hideturtle()
+colormode(255)
+turtle.color(255, 255, 255)
+bgcolor(200, 200, 200)
+
 
 colors = extract(
     "A:/luiz-vidal/github/100-days-of-code/day-018-intermediate-turtle-and-the-graphical-user-interface-gui/07-the-hirst-painting-project/image.png", 10
 )
+
+color_list = []
 
 
 for color in colors:
     r = color.rgb.r
     g = color.rgb.g
     b = color.rgb.b
-    rgb_colors.append((r, g, b))
+    color_list.append((r, g, b))
 
+turtle.penup()
+turtle.goto(-225, -225)
 
-print(rgb_colors)
+for _ in range(10):
+    for _ in range(10):
+        turtle.dot(20, choice(color_list))
+        turtle.forward(50)
+
+    y = turtle.position()[1]
+
+    turtle.goto(-225, y + 50)
+
+screen = Screen()
+screen.exitonclick()
---
+++ day-018-intermediate-turtle-and-the-graphical-user-interface-gui/07-the-hirst-painting-project/my-solution.py
@@ -0,0 +1,61 @@
+from turtle import Turtle, Screen
+from random import choice
+from colorgram import extract
+
+
+turtle = Turtle()
+turtle.speed("fastest")
+
+hex_colors = []
+
+
+def clamp(x):
+    return max(0, min(x, 255))
+
+
+def rgb_to_hex(rgb):
+    return "#{0:02x}{1:02x}{2:02x}".format(
+        clamp(rgb[0]), clamp(rgb[1]), clamp(rgb[2]))
+
+
+colors = extract(
+    "A:/luiz-vidal/github/100-days-of-code/day-018-intermediate-turtle-and-the-graphical-user-interface-gui/07-the-hirst-painting-project/image.png", 10
+)
+
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    hex_color = rgb_to_hex((r, g, b))
+    hex_colors.append(hex_color)
+
+
+def change_color():
+    color = choice(hex_colors)
+    turtle.color(color)
+
+
+def draw_dots(quantity):
+    for _ in range(quantity):
+        change_color()
+        turtle.pendown()
+        turtle.dot(20)
+        turtle.penup()
+        turtle.forward(50)
+
+
+def draw_hirst(size):
+
+    for _ in range(size):
+        draw_dots(size)
+        y = turtle.position()[1]
+
+        turtle.goto(0, y + 50)
+
+
+draw_hirst(10)
+
+
+screen = Screen()
+screen.exitonclick()
