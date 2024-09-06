8084716f233ef92b9364bada50560531b741c9f5
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-05 21:05:22 -0300

    :construction: 020-150:How to Control the Snake with a Keypress

---
+++ day-020-intermediate-build-the-snake-game-part-1-animation-and-coordinates/04-how-to-control-the-snake-with-a-keypress/main.py
@@ -0,0 +1,32 @@
+from turtle import Screen
+from time import sleep
+from snake import Snake
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
+screen.update()
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    sleep(0.1)
+
+    snake.move()
+
+
+screen.exitonclick()
