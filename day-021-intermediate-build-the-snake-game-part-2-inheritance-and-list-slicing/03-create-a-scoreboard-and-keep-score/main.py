d75293ef196e192929a033968a099d96412f3891
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-07 13:02:09 -0300

    :construction: 021-155:Create a Scoreboard and Keep Score

---
+++ day-021-intermediate-build-the-snake-game-part-2-inheritance-and-list-slicing/03-create-a-scoreboard-and-keep-score/main.py
@@ -0,0 +1,39 @@
+from turtle import Screen
+from time import sleep
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score_board = Scoreboard()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
+screen.update()
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    sleep(0.1)
+    snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score_board.increase_score()
+
+
+screen.exitonclick()
