b3603d6d5fea08920db48ff1a9dad016f55720f2
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-07 15:56:26 -0300

    :construction: 022-162:Create a Paddle that responds to Key Presses

---
+++ day-022-intermediate-build-pong-the-famous-arcade-game/02-create-a-paddle-that-responds-to-key-presses/main.py
@@ -0,0 +1,37 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+screen.listen()
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.onkey(key="Up", fun=go_up)
+screen.onkey(key="Down", fun=go_down)
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
+
+screen.exitonclick()
