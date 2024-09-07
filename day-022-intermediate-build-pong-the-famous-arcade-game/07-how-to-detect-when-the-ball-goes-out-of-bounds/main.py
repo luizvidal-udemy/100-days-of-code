552dea15922b643325bd2984c623f8cf047800a9
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-07 18:55:06 -0300

    :construction: 022-167:How to Detect when the Ball goes Out of Bounds

---
+++ day-022-intermediate-build-pong-the-famous-arcade-game/07-how-to-detect-when-the-ball-goes-out-of-bounds/main.py
@@ -0,0 +1,48 @@
+from turtle import Screen
+from time import sleep
+from paddle import Paddle
+from ball import Ball
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+screen.listen()
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.onkey(key="Up", fun=r_paddle.go_up)
+screen.onkey(key="Down", fun=r_paddle.go_down)
+
+screen.onkey(key="w", fun=l_paddle.go_up)
+screen.onkey(key="s", fun=l_paddle.go_down)
+
+game_is_on = True
+
+while game_is_on:
+    sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detect collision with a wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
+
+screen.exitonclick()
