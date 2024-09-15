60f267b01174bb7387bfd3aecb83ff24663c9395
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-15 17:12:54 -0300

    :construction: 026-198: Apply List Comprehension to the U.S. States Game

---
+++ day-026-intermediate-list-comprehension-and-the-nato-alphabet/03-apply-list-comprehension-to-the-us-states-game/main.py
@@ -0,0 +1,48 @@
+import turtle
+import pandas
+from pathlib import Path
+
+CURRENT_DIR = Path(__file__).parent
+
+IMAGE_PATH = str(CURRENT_DIR / "blank_states_img.gif")
+
+DATA_PATH = str(CURRENT_DIR / "50_states.csv")
+
+OUTPUT_PATH = str(CURRENT_DIR / "states_to_learn.csv")
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.addshape(IMAGE_PATH)
+turtle.shape(IMAGE_PATH)
+
+data = pandas.read_csv(DATA_PATH)
+
+all_states = data.state.to_list()
+
+guessed_states = []
+
+
+while len(guessed_states) < len(all_states):
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/50 States Correct",
+        prompt="What's another state's name?:"
+    )
+
+    answer_state = str(answer_state).title()
+
+    if answer_state == "Exit":
+        missing_states = [
+            state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv(OUTPUT_PATH)
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        coordinates = (state_data.x.item(), state_data.y.item())
+        t.goto(coordinates)
+        t.write(answer_state)
