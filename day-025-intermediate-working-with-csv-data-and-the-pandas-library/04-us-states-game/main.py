ec05fedb196c4f53c941adb765b1c80520f34d3e
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-15 15:34:06 -0300

    :construction: 025-194: U.S. States Game Part 2:Challenge with .csv

--- day-025-intermediate-working-with-csv-data-and-the-pandas-library/04-us-states-game/main.py
+++ day-025-intermediate-working-with-csv-data-and-the-pandas-library/04-us-states-game/main.py
@@ -1,15 +1,42 @@
 import turtle
+import pandas
 from pathlib import Path
 
 CURRENT_DIR = Path(__file__).parent
+
 IMAGE_PATH = str(CURRENT_DIR / "blank_states_img.gif")
+
+DATA_PATH = str(CURRENT_DIR / "50_states.csv")
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 screen.addshape(IMAGE_PATH)
 turtle.shape(IMAGE_PATH)
 
-answer_state = screen.textinput(
-    title="Guess the State", prompt="What's another state's name?:")
+data = pandas.read_csv(DATA_PATH)
+
+all_states = data.state.to_list()
+
+guessed_states = []
+
+
+while len(guessed_states) < len(all_states):
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/50 States Correct",
+        prompt="What's another state's name?:"
+    )
+
+    answer_state = str(answer_state).title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        coordinates = (state_data.x.item(), state_data.y.item())
+        t.goto(coordinates)
+        t.write(answer_state)
+
 
 screen.exitonclick()
