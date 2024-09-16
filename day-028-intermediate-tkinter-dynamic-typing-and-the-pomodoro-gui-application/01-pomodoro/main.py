7cfc27a04d0f08ce7b09329e401c139bdbeca04f
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-16 18:52:45 -0300

    :construction: 028-214: How to work with the Canvas Widget and Add Images to Tkinter

---
+++ day-028-intermediate-tkinter-dynamic-typing-and-the-pomodoro-gui-application/01-pomodoro/main.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from pathlib import Path
+
+# ---------------------------- CONSTANTS ------------------------------- #
+CURRENT_DIR = Path(__file__).parent
+IMAGE_PATH = CURRENT_DIR / "tomato.png"
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+tomato_img = PhotoImage(file=IMAGE_PATH)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
+canvas.pack()
+
+window.mainloop()
