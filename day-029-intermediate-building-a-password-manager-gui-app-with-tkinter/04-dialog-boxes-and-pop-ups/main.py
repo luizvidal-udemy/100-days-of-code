3743ff677917aacd25bf24cc1ae4f7a1df24ad2e
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-23 16:13:26 -0300

    :construction: 029:225: Dialog boxes and pop-ups

--- day-029-intermediate-building-a-password-manager-gui-app-with-tkinter/04-dialog-boxes-and-pop-ups/main.py
+++ day-029-intermediate-building-a-password-manager-gui-app-with-tkinter/04-dialog-boxes-and-pop-ups/main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from tkinter import *
+from tkinter import messagebox
 
 
 CURRENT_DIR = Path(__file__).parent
@@ -16,13 +17,24 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open(DATA_PATH, "a") as file:
-        file.write(
-            f"{website} | {email} | {password}\n"
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(
+            title="Oops", message="Please make sure you haven't left any fields empty.")
+
+    else:
+
+        is_ok = messagebox.askokcancel(
+            title=website, message=f"The details entered are: \nEmail: {email}\nPassword: {password} \nIs it ok to save?"
         )
 
-    website_entry.delete(0, END)
-    password_entry.delete(0, END)
+        if is_ok:
+            with open(DATA_PATH, "a") as file:
+                file.write(
+                    f"{website} | {email} | {password}\n"
+                )
+
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------    ------------------- #
